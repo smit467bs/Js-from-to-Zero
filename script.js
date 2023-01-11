4bae61c33c11e389aabd0f265c3742f8ecd86dcc
smit467bs <smit467bs@gmail.com>
2023-01-11 14:30:48 +0300

    init cv

---
+++ script.js
@@ -0,0 +1,7 @@
+<li>Siemens TiaPortal, SiemensHmi</li>
+<li>Owen PLC, Owen HMI</li>
+<li>HTML/HTML5, CSS3/SCSS</li>
+<li>Hardworking</li>
+<li>Responsible</li>
+<li>Sociable</li>
+<li>Quick learner</li> 
